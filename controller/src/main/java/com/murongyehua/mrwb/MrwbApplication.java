11dc0b7d0bb6beefa4cd9b1346dd0413af309565
murongyehua <11371564@qq.com>
2020-06-15 19:10:44 +0800

    init

---
+++ controller/src/main/java/com/murongyehua/mrwb/MrwbApplication.java
@@ -0,0 +1,16 @@
+package com.murongyehua.mrwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author murongyehua
+ */
+@SpringBootApplication
+public class MrwbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrwbApplication.class, args);
+    }
+
+}
