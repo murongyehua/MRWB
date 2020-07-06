6d3aadc7859246d3e5aa6e0f048b810c5c26752a
murongyehua <11371564@qq.com>
2020-07-06 17:28:12 +0800

    dev

--- MRWorkBox/src/components/page/Login.vue
+++ MRWorkBox/src/components/page/Login.vue
@@ -4,14 +4,14 @@
             <div class="ms-title">MR Work Box</div>
             <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
                 <el-form-item prop="username">
-                    <el-input v-model="param.username" placeholder="username">
+                    <el-input v-model="param.username" placeholder="请输入用户名">
                         <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
                     </el-input>
                 </el-form-item>
                 <el-form-item prop="password">
                     <el-input
                         type="password"
-                        placeholder="password"
+                        placeholder="请输入密码"
                         v-model="param.password"
                         @keyup.enter.native="submitForm()"
                     >
